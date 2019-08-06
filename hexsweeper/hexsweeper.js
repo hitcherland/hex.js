37e4b615c154ceab8eec70307a176889f750ea85
hitcherland <hitcherland@gmail.com>
2019-08-06 22:45:28 +0100

    in progress to adding navigation tools

--- hexsweeper/hexsweeper.js
+++ hexsweeper/hexsweeper.js
@@ -1,4 +1,4 @@
-var render = function(hexagon) {
+var render = function(ctx, hexagon) {
     var cx = ctx.canvas.width / 2.0;
     var cy = ctx.canvas.height / 2.0;
     var pos = hexagon.node.to_cartesian(hexRadius);
@@ -18,13 +18,11 @@ var render = function(hexagon) {
 function resize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    hexRadius = grid.calculate_hex_radius(canvas.width * 0.8, canvas.height * 0.8, zoom);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    hexRadius = grid.calculate_hex_radius(canvas.width * 0.8, canvas.height * 0.8, grid.zoom);
     grid.render();
 }
 
 var prevDiff = undefined;
-var zoom = 1.0;
 function pointermove(e) {
     var pos = { 
         x: e.clientX - canvas.width / 2.0,
@@ -33,21 +31,24 @@ function pointermove(e) {
 
     for(var i=0; i<touches.length; i++) {
         var ev = touches[i];
-        if(ev.pointerId == e.pointerId) {
+        if(ev.identifier == e.identifier) {
             touches[i] = ev;
         }
     }
 
-    Object.values(grid.hexagons).filter(x => x.state & 1 == 1).forEach(x => x.state -= 1)
+    Object.values(grid.hexagons)
+          .filter(x => x.state & 1 == 1)
+          .forEach(x => x.state -= 1)
 
     if(touches.length == 2) {
         var curDiff = Math.abs(touches[0].clientX - touches[1].clientX);
         if(prevDiff > 0) {
             if(curDiff > prevDiff) {
-                console.log(curDiff);
+                zoomControl(3)
                 //zoom in
             }
             if(curDiff < prevDiff) {
+                zoomControl(-3)
                 //zoom out
             }
 
@@ -55,58 +56,81 @@ function pointermove(e) {
         prevDiff = curDiff;
 
     } else {
-
         var coord = hex.HexNode.from_cartesian(pos, hexRadius);
         var hexagon = grid.hexagons[coord];
         if(hexagon !== undefined) {
             hexagon.state += 1
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
         }
     }
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     grid.render();
 }
 
 function pointerdown(e) {
     touches.push(e)
+    grid.render();
 }
 
 function pointerup(e) {
     for(var i=0; i<touches.length; i++) {
         var ev = touches[i];
-        if(ev.pointerId == e.pointerId) {
+        if(ev.identifier == e.identifier) {
             touches.splice(i, 1);
         }
     }
+
+    Object.values(grid.hexagons)
+          .filter(x => x.state & 1 == 1)
+          .forEach(x => x.state -= 1)
+
+    grid.render();
 }
 
-
+function zoomControl(d) {
+    grid.zoom -= d / 30.0;
+    if(grid.zoom < 1)
+        grid.zoom = 1
+    resize();
+}
 
 var states = {
     'UNREVEALED': 0,
     'UNREVEALED_HOVER': 1,
+    'UNREVEALED_ACTIVE': 2,
 }
 
 var styles = {
     0: {'stroke': '#000', 'fill': '#ccc'},
-    1: {'stroke': '#222', 'fill': '#ddd'},
+    1: {'stroke': '#222', 'fill': '#f00'},
+    2: {'stroke': '#222', 'fill': '#0f0'},
 }
 
 var canvas = document.getElementById('game');
-var ctx = canvas.getContext('2d');
-var grid = new hex.HexGrid(2, render);
-var hexRadius = grid.calculate_hex_radius(canvas.width, canvas.height, zoom);
+var grid = new hex.HexGrid(canvas, 2, render, {
+    'tap': (h) => {console.log(h); h.state = 2; grid.render();},
+    'pinch': (d) => {
+        zoomControl(d);
+    },
+    'pan': (d) => {
+    },
+});
+
+var hexRadius = grid.calculate_hex_radius(canvas.width, canvas.height, grid.zoom);
 
 var touches = [];
 
-canvas.addEventListener('pointerdown', pointerdown);
-canvas.addEventListener('pointermove', pointermove);
-canvas.addEventListener('wheel', e => {
-    zoom -= e.deltaY / 30.0;
-    if(zoom < 0.1)
-        zoom = 0.1
-    resize();
-});
+/*
+canvas.addEventListener('touchend', (e) => pointerup(e.changedTouches[0]), false);
+canvas.addEventListener('touchcancel', e => pointerup(e.touches[0]), false);
+canvas.addEventListener('mouseup', pointerup, false);
+
+canvas.addEventListener('touchstart', e => pointerdown(e.touches[0]), false);
+canvas.addEventListener('mousedown', pointerdown, false);
+
+canvas.addEventListener('touchmove', e => pointermove(e.touches[0]), false);
+canvas.addEventListener('mousemove', pointermove, false);
+canvas.addEventListener('wheel', e => zoomControl(e.deltaY));
+*/
+
 window.addEventListener('resize', resize);
 
 resize();
--- src/grid.js
+++ src/grid.js
@@ -1,18 +1,76 @@
+var Hammer = require('hammerjs');
 var node = require('./node.js');
 
 const SQRT_3 = Math.sqrt(3.0);
 
 class HexGrid {
-    constructor(radius, renderFunction) {
+    constructor(canvas, radius, renderFunction, events) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
         radius = radius === undefined ? 5 : radius;
 
         this.radius = radius;
         this.renderFunction = renderFunction;
         this.hexagons = {}
+        this.zoom = 1;
         new node.HexNode(0, 0).area(radius).forEach(hex => { return this.hexagons[hex.toString()] = {
             node: hex,
             state: 0,
         }});
+
+        this.mc = new Hammer.Manager(this.canvas, {
+            recognizers: [
+                [Hammer.Pinch],
+                [Hammer.Tap],
+                [Hammer.Pan],
+            ]
+        });
+
+        function dist(x1, y1, x2, y2) {
+            return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+        }
+
+        var this_ = this;
+        var zoom_start = undefined;
+        if(events.pinch !== undefined) {
+            this.mc.on('pinch', function(e) {
+                var d = dist(e.pointers[0].screenX,
+                             e.pointers[0].screenY,
+                             e.pointers[1].screenX,
+                             e.pointers[1].screenY);
+                if(zoom_start !== undefined) {
+                    if(d > zoom_start)
+                        events.pinch(3 * - d / zoom_start);
+
+                    if(d < zoom_start)
+                        events.pinch(3 * zoom_start / d);
+                }
+                zoom_start = d;
+
+            });
+        }
+
+        if(events.tap !== undefined) {
+            this.mc.on('tap', function(e) {
+                var cartesian = e.center;
+                cartesian.x -= this_.canvas.width / 2.0;
+                cartesian.y -= this_.canvas.height / 2.0;
+                cartesian.x /= this_.zoom;
+                cartesian.y /= this_.zoom;
+                var hr = this_.calculate_hex_radius(this_.canvas.width, this_.canvas.height);
+                var coord = node.HexNode.from_cartesian(cartesian, hr);
+                if(this_.hexagons[coord] !== undefined) {
+                    events.tap(this_.hexagons[coord]);
+                }
+            });
+        }
+
+        if(events.pan !== undefined) {
+            this.mc.on('pan', function(e) {
+                var c = e.center;
+                console.log("pan");
+            });
+        }
     }
 
     calculate_hex_radius(width, height, zoom) {
@@ -24,6 +82,8 @@ class HexGrid {
     }
 
     render() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         Object.values(this.hexagons).sort((a, b) => { 
             var ay = a.node.y(1);
             var by = b.node.y(1);
@@ -32,7 +92,7 @@ class HexGrid {
             } else {
                 return -1;
             }
-        }).forEach(hexagon => this.renderFunction(hexagon));
+        }).forEach(hexagon => this.renderFunction(this.ctx, hexagon));
     }
 }
 
