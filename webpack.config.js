3e8da3950992a858901af99716ad7086ee71faee
hitcherland <hitcherland@gmail.com>
2019-08-07 15:31:37 +0100

    updated for npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 module.exports = {
+    devtool: 'source-map',
+    mode: 'production',
     entry: {
         hex: './src/index.js',
     },
--- webpack.gh-pages.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const merge = require('webpack-merge');
-const common = require('./webpack.production.js');
-
-module.exports = merge(common, {
-    output: {
-        path: path.resolve(__dirname, "gh-pages"),
-    }
-});
